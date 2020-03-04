579813d14872555d96a3560025d491c67a910206
William Lin <willlin1996@gmail.com>
2020-03-04 20:18:11 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,13 +1,4 @@
 # Your Code Here
 def map(array) 
-  new_array = 0
-  index = 0 
-  while index < array.length do
-  array.do |x| 
-    x
-  yield(array[index])
-  index += 1
-end 
-new_array
-end
+  
 
